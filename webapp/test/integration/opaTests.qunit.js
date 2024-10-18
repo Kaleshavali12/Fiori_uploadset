77f20580e4a157e80e58898d3db93a773d5e9ac4
Kalesha Vali Pattan <kaleshavali.pattan@ust.com>
2024-10-18 09:34:43 +0000

    first v for uploadset fiori

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/upload/fioriupload/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comupload/fioriupload/test/unit/controller/fioriupload.controller"
+], function () {
+	"use strict";
+});
